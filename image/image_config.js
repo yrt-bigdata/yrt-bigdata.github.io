5d2fb6e73e88f463878a7ce216ddfe090f96d73c
yrt-bigdata <2125603137@qq.com>
2026-04-23 19:47:15 +0800

    Update image_config.js

--- image/image_config.js
+++ image/image_config.js
@@ -4,35 +4,35 @@
 const imageConfig = {
     // 个人头像
     profile: {
-        placeholder: 'images/profile_placeholder.jpg',
+        placeholder: 'image/profile_placeholder.jpg',
         alt: '个人头像'
     },
     
     // 项目图片
     projects: {
-        project1: 'images/project1_placeholder.jpg',
-        project2: 'images/project2_placeholder.jpg',
-        project3: 'images/project3_placeholder.jpg'
+        project1: 'image/project1_placeholder.jpg',
+        project2: 'image/project2_placeholder.jpg',
+        project3: 'image/project3_placeholder.jpg'
     },
     
     // 证书图片
     certificates: {
-        cert1: 'images/certificate1_placeholder.jpg',
-        cert2: 'images/certificate2_placeholder.jpg',
-        cert3: 'images/certificate3_placeholder.jpg'
+        cert1: 'image/certificate1_placeholder.jpg',
+        cert2: 'image/certificate2_placeholder.jpg',
+        cert3: 'image/certificate3_placeholder.jpg'
     },
     
     // 教育背景相关图片
     education: {
-        diploma: 'images/diploma_placeholder.jpg',
-        school: 'images/school_placeholder.jpg'
+        diploma: 'image/diploma_placeholder.jpg',
+        school: 'image/school_placeholder.jpg'
     },
     
     // 工作经历相关图片
     experience: {
-        company1: 'images/company1_placeholder.jpg',
-        company2: 'images/company2_placeholder.jpg',
-        company3: 'images/company3_placeholder.jpg'
+        company1: 'image/company1_placeholder.jpg',
+        company2: 'image/company2_placeholder.jpg',
+        company3: 'image/company3_placeholder.jpg'
     }
 };
 
